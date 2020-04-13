b191460813319f4d83c0c9babcc1b349c4a6035f
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2020-04-13 16:51:21 +0300

    feat: define names of future object methods

--- unit-tests/data.py
+++ unit-tests/data.py
@@ -1,2 +1,25 @@
 class Data(object):
     def __init__(self):
+        self.string = ''
+
+    def add(self, s):
+        pass
+
+    def toUpperCase(self, s):
+        pass
+
+    def toLowerCase(self, s):
+        pass
+
+    def capitalizeFirstCharacter(self, s):
+        pass
+
+    def capitalizeFirstCharacterOfEachWord(self, s):
+        pass
+
+    def countObjectInString(self, s, target):
+        pass
+
+    def endswith(self, s, target):
+        pass
+
