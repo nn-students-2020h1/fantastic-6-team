d09e42e27930e1d6ddc7ba8400c017da3f4c5808
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2020-04-13 17:28:32 +0300

    refactor: delete odd methods

--- data.py
+++ data.py
@@ -2,24 +2,14 @@ class Data(object):
     def __init__(self):
         self.string = ''
 
- #   def add(self, s):
- #       pass
-
     def to_upper_case(self, s):
         pass
 
     def to_lower_case(self, s):
         pass
 
-    def capitalize_first_character(self, s):
+    def countObjectInString(self, s, target):
         pass
 
-    def capitalize_first_character_of_each_word(self, s):
+    def ends_with(self, s, target):
         pass
-
-# def countObjectInString(self, s, target):
-#       pass
-
-#   def ends_with(self, s, target):
-#        pass
-
