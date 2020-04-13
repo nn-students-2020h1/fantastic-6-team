87c1a921cdb1187cfb19e35b50565a070acb9c50
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2020-04-13 17:54:25 +0300

    feat: add tests for different data types for to_upper_case method

--- tests/test_data.py
+++ tests/test_data.py
@@ -52,12 +52,47 @@ class TestStringMethods(unittest.TestCase):
             self.data.to_lower_case({'hello': 'hi'})
 
     def test_to_upper_case_string(self):
-        self.data.to_upper_case('hello World!')
+        self.data.to_upper_case('HELLO World!')
         self.assertEqual(self.data.string, 'HELLO WORLD!')
+        self.assertNotEqual(self.data.string, 'HELLO World!')
+        self.assertTrue(self.data.string.islower())
+        self.assertFalse(self.data.string.isupper())
 
-    def test_to_lower_case_number(self):
+    def test_to_upper_case_int(self):
         with self.assertRaises(TypeError):
             self.data.to_upper_case(5)
+
+    def test_to_upper_case_float(self):
+        with self.assertRaises(TypeError):
+            self.data.to_upper_case(5.9)
+
+    def test_to_upper_case_bool(self):
+        with self.assertRaises(TypeError):
+            self.data.to_upper_case(True)
+
+    def test_to_upper_case_list(self):
+        with self.assertRaises(TypeError):
+            self.data.to_upper_case([2, 'something'])
+
+    def test_to_upper_case_tupple(self):
+        with self.assertRaises(TypeError):
+            self.data.to_upper_case([2, 'something'])
+
+    def test_to_upper_case_set(self):
+        with self.assertRaises(TypeError):
+            self.data.to_upper_case({4, 5})
+
+    def test_to_upper_case_empty_list(self):
+        with self.assertRaises(TypeError):
+            self.data.to_upper_case([])
+
+    def test_to_upper_case_none(self):
+        with self.assertRaises(TypeError):
+            self.data.to_upper_case(None)
+
+    def test_to_upper_case_dict(self):
+        with self.assertRaises(TypeError):
+            self.data.to_upper_case({'hello': 'hi'})
 
 
 if __name__ == '__main__':
