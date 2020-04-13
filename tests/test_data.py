e1e4c247a751cf27ed1acf1f40ea5e60666c755b
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2020-04-13 17:49:14 +0300

    feat: add tests for different data types for to_lower_case method

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,7 +1,7 @@
 import unittest
 from data import Data
 
-class TestData(unittest.TestCase):
+class TestStringMethods(unittest.TestCase):
     def setUp(self):
         self.data = Data()
 
@@ -11,16 +11,51 @@ class TestData(unittest.TestCase):
     def test_to_lower_case_string(self):
         self.data.to_lower_case('HELLO World!')
         self.assertEqual(self.data.string, 'hello world!')
+        self.assertNotEqual(self.data.string, 'HELLO World!')
+        self.assertTrue(self.data.string.islower())
+        self.assertFalse(self.data.string.isupper())
 
-    def test_to_lower_case_string(self):
+    def test_to_lower_case_int(self):
         with self.assertRaises(TypeError):
             self.data.to_lower_case(5)
+
+    def test_to_lower_case_float(self):
+        with self.assertRaises(TypeError):
+            self.data.to_lower_case(5.9)
+
+    def test_to_lower_case_bool(self):
+        with self.assertRaises(TypeError):
+            self.data.to_lower_case(True)
+
+    def test_to_lower_case_list(self):
+        with self.assertRaises(TypeError):
+            self.data.to_lower_case([2, 'something'])
+
+    def test_to_lower_case_tupple(self):
+        with self.assertRaises(TypeError):
+            self.data.to_lower_case([2, 'something'])
+
+    def test_to_lower_case_set(self):
+        with self.assertRaises(TypeError):
+            self.data.to_lower_case({4, 5})
+
+    def test_to_lower_case_empty_list(self):
+        with self.assertRaises(TypeError):
+            self.data.to_lower_case([])
+
+    def test_to_lower_case_none(self):
+        with self.assertRaises(TypeError):
+            self.data.to_lower_case(None)
+
+    def test_to_lower_case_dict(self):
+        with self.assertRaises(TypeError):
+            self.data.to_lower_case({'hello': 'hi'})
 
     def test_to_upper_case_string(self):
         self.data.to_upper_case('hello World!')
         self.assertEqual(self.data.string, 'HELLO WORLD!')
 
-    def test_to_lower_case_string(self):
+    def test_to_lower_case_number(self):
         with self.assertRaises(TypeError):
             self.data.to_upper_case(5)
 
@@ -29,8 +64,8 @@ if __name__ == '__main__':
     unittest.main()
 
 # Add tests:
-#.assertEqual()++ / .assertNotEqual()
-#.assertTrue() / .assertFalse
+#.assertEqual()++ / .assertNotEqual()++
+#.assertTrue()++ / .assertFalse++
 #.assertIs() / .assertIsNot()
 #.assertIn() / .assertNotIn()
 #.assertIsNone() / .assertIsNotNone()
