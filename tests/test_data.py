6a459612cf6302a659fa38c4f7763ce9a0b3ae59
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2020-04-13 19:21:26 +0300

    feat: add messages for assertRaises

--- tests/test_data.py
+++ tests/test_data.py
@@ -22,39 +22,39 @@ class TestStringMethods(unittest.TestCase):
         self.assertIsNotNone(self.data.string)
 
     def test_to_lower_case_int(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.to_lower_case(5)
 
     def test_to_lower_case_float(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.to_lower_case(5.9)
 
     def test_to_lower_case_bool(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.to_lower_case(True)
 
     def test_to_lower_case_list(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.to_lower_case([2, 'something'])
 
     def test_to_lower_case_tupple(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.to_lower_case([2, 'something'])
 
     def test_to_lower_case_set(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.to_lower_case({4, 5})
 
     def test_to_lower_case_empty_list(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.to_lower_case([])
 
     def test_to_lower_case_none(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.to_lower_case(None)
 
     def test_to_lower_case_dict(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.to_lower_case({'hello': 'hi'})
 
     # to_upper_case methods tests
@@ -69,39 +69,39 @@ class TestStringMethods(unittest.TestCase):
         self.assertIsNotNone(self.data.string)
 
     def test_to_upper_case_int(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.to_upper_case(5)
 
     def test_to_upper_case_float(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.to_upper_case(5.9)
 
     def test_to_upper_case_bool(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.to_upper_case(True)
 
     def test_to_upper_case_list(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.to_upper_case([2, 'something'])
 
     def test_to_upper_case_tupple(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.to_upper_case([2, 'something'])
 
     def test_to_upper_case_set(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.to_upper_case({4, 5})
 
     def test_to_upper_case_empty_list(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.to_upper_case([])
 
     def test_to_upper_case_none(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.to_upper_case(None)
 
     def test_to_upper_case_dict(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.to_upper_case({'hello': 'hi'})
 
     # count_number_of_object_in_string methods tests
@@ -117,39 +117,39 @@ class TestStringMethods(unittest.TestCase):
 
 
     def test_count_number_of_object_int(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.count_number_of_object(5)
 
     def test_count_number_of_object_float(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.count_number_of_object(5.9)
 
     def test_count_number_of_object_bool(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.count_number_of_object(True)
 
     def test_count_number_of_object_list(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.count_number_of_object([2, 'something'])
 
     def test_count_number_of_object_tupple(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.count_number_of_object([2, 'something'])
 
     def test_count_number_of_object_set(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.count_number_of_object({4, 5})
 
     def test_count_number_of_object_list(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.count_number_of_object([])
 
     def test_count_number_of_object_none(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.count_number_of_object(None)
 
     def test_count_number_of_object_dict(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Data type of input data should be string'):
             self.data.count_number_of_object({'hello': 'hi'})
 
 if __name__ == '__main__':
