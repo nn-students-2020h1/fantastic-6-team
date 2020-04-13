8286c8b0a39efa2cbac7ac5d4eb7145b8a998899
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2020-04-13 16:26:23 +0300

    refactor: add technical task

--- unit-tests/tests/test_data.py
+++ unit-tests/tests/test_data.py
@@ -1,10 +1,22 @@
 import unittest
 from data import Data
 
-class MyTestCase(unittest.TestCase):
+class TestData(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
+        self.data = Data()
+    def tearDown(self):
+        self.data.data_list = []
 
 
 if __name__ == '__main__':
     unittest.main()
+
+# Add tests:
+#.assertEqual() / .assertNotEqual()
+#.assertTrue() / .assertFalse
+#.assertIs() / .assertIsNot()
+#.assertIn() / .assertNotIn()
+#.assertIsNone() / .assertIsNotNone()
+#.assertIsInstance() / .assertNotInstance()
+#.assertWarns()
+#.assertRaises()
